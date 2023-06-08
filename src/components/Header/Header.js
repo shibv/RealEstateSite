4c5fd2bc0b9624e8f0cf65fb587a6e1e0e4377e1
Shiv <ps431352@gmail.com>
2023-06-08 18:39:17 +0530

    "last Changes"

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,24 +1,45 @@
 import "./Header.css"
-import React from "react";
+import React, { useState } from "react";
+import {BiMenuAltRight} from "react-icons/bi"
+import { HiFaceSmile } from "react-icons/hi2";
+import OutsideClickHandler from 'react-outside-click-handler';
 
 
 const Header = () => {
+  const [menuOpen,setmenuOpen] = useState(false);
+
+  function getMenuStyles(menuOpen)
+  {
+      if(document.documentElement.clientWidth <= 800)
+      {
+         return {
+          right: !menuOpen && "-100%" 
+         }
+      }
+  }
   return (
     <section className="h-wrapper">
       <div className=" flexCenter paddings innerWidth h-container">
         <img src="./logo.png" alt="logo" width={100} />
-     
-      <div className=" flexCenter h-menu">
+     <OutsideClickHandler onOutsideClick={() => {setmenuOpen(false)}}>
+      
+     <div className=" flexCenter h-menu" style={getMenuStyles(menuOpen)}>
         <a href="" >Residencies</a>
         <a href="" >Our Value</a>
         <a href="" >Contact Us</a>
         <a href="" >Get Started</a>
         <button className="button">
         <a href="">Contact</a>
-        </button>
-        
+        </button> 
+       
+      </div>
+     </OutsideClickHandler>
+     
+      <div className="menu-icon" onClick={() => setmenuOpen((prev) => !prev)}>
+        <BiMenuAltRight size={40}/>
       </div>
       </div>
+      
     </section>
   );
 };
--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,7 +1,8 @@
 import React from "react";
-import "./Hero.css"
-import {HiLocationMarker} from "react-icons/hi"
-import CountUp from "react-countup"
+import "./Hero.css";
+import { HiLocationMarker } from "react-icons/hi";
+import CountUp from "react-countup";
+import { motion, spring } from "framer-motion";
 
 const Hero = () => {
   return (
@@ -10,55 +11,66 @@ const Hero = () => {
         <div className="flexColStart hero-left">
           <div className="hero-title">
             <div className="orange-circle"></div>
-            <h1>Discover <br /> Most Suitable <br /> Property</h1>
+            <motion.h1
+            initial={{y:"2em" , opacity:0} }
+            animate={{y:0, opacity:1}}
+            transition={{
+              duration:2,
+              type:"spring",
+
+            }}
+            >
+              Discover <br /> Most Suitable <br /> Property
+            </motion.h1>
           </div>
           <div className="flexColStart secondaryText hero-desc">
-            <span>Find a variety of properties that suit you very easilty </span>
-           
+            <span>
+              Find a variety of properties that suit you very easilty{" "}
+            </span>
+
             <span>Forget all difficulties in finding a residence for you </span>
           </div>
-          
+
           <div className="flexCenter search-bar">
-            <HiLocationMarker  color="var(--blue)" size={25}></HiLocationMarker>
-            <input type="text"  />
-            <button className="button" >Search</button>
+            <HiLocationMarker color="var(--blue)" size={25}></HiLocationMarker>
+            <input type="text" />
+            <button className="button">Search</button>
           </div>
           <div className="flexCenter stats">
-             <div className="flexColCenter stat">
+            <div className="flexColCenter stat">
               <span>
-                <CountUp start={8000} end={9000} duration={4} ></CountUp>
+                <CountUp start={8000} end={9000} duration={4}></CountUp>
                 <span>+</span>
               </span>
-              <span className="secondaryText">
-                Premium Products
-              </span>
-             </div>
-             <div className="flexColCenter stat">
+              <span className="secondaryText">Premium Products</span>
+            </div>
+            <div className="flexColCenter stat">
               <span>
-                <CountUp start={1940} end={2000} duration={4} ></CountUp>
+                <CountUp start={1940} end={2000} duration={4}></CountUp>
                 <span>+</span>
               </span>
-              <span className="secondaryText">
-                Happy Costumer
-              </span>
-             </div>
-             <div className="flexColCenter stat">
+              <span className="secondaryText">Happy Costumer</span>
+            </div>
+            <div className="flexColCenter stat">
               <span>
-                <CountUp  end={28} ></CountUp>
+                <CountUp end={28}></CountUp>
                 <span>+</span>
               </span>
-              <span className="secondaryText">
-                Award Winning
-              </span>
-             </div>
-             
+              <span className="secondaryText">Award Winning</span>
+            </div>
           </div>
-
         </div>
         <div className="flexCenter hero-right">
-          <div className="image-conatiner">
+          <motion.div className="image-conatiner"
+           initial={{x:"7em", opacity:0}}
+           animate={{x:0, opacity:1}}
+           transition={{
+            duration:2,
+            type:spring
+           }}
+          >
             <img src="./hero-image.png" alt="" height="100%" width="100%" />
-          </div>
+          </motion.div>
         </div>
       </div>
     </section>
